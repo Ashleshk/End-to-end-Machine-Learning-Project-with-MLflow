d6702578a0106f9991138948380518e821c8ad67
Ashlesh Khajbage <ashleshuk@gmail.com>
2024-06-02 18:26:08 -0500

    config changes

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -103,7 +103,7 @@ class ConfigurationManager:
             all_params=params,
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
-            mlflow_uri="https://dagshub.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow.mlflow",
+            mlflow_uri="https://dagshub.com/ashleshuk/End-to-end-Machine-Learning-Project-with-MLflow.mlflow",
            
         )
 
